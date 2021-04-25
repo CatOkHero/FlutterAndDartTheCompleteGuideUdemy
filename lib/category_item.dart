a6639aeb109d56de43c9fade5c27731394e97955
Roman Hapatyn <r.hapatyn@tawasal.ae>
2021-04-25 10:53:21 +0300

    init commit

---
+++ lib/category_item.dart
@@ -0,0 +1,42 @@
+import 'package:cooking_app/models/category.dart';
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  CategoryItem(this.category);
+
+  void selectCategory(BuildContext ctx, Category item) {
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: category,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context, category),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.7),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:cooking_app/category_meals_screen.dart';
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TestMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.white,
+        canvasColor: Colors.white,
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromARGB(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromARGB(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealScreen(),
+      },
+    );
+  }
+}
