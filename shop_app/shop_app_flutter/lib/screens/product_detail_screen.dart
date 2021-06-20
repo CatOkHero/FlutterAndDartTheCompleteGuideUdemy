8e081c8c0648c6a5ed4e5c4f72e064e65cdd48e3
Roman Hapatyn <roman.hapatyn@gmail.com>
2021-06-20 16:15:14 +0300

    Init commit for shopping_app. Added 1 screen + ttf

---
+++ shop_app/shop_app_flutter/lib/screens/product_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/models/product.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  final Product _product;
+
+  ProductDetailScreen(this._product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
